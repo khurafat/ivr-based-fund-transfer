fbc3de8d1dc0a3357c6c1db7d03aa1a846cbe8c1
Subham banga <subhambanga26@gmail.com>
2017-09-10 06:46:49 +0530

    fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -26,7 +26,7 @@
       @foreach ($customers as $customer)
 
         <tr>
-          <td class="link"><b><a href="/customer/{{$customer->id}}">{{$customer->id}}</a></b></td>
+          <td class="link"><b><a href="/customers/{{$customer->id}}">{{$customer->id}}</a></b></td>
           <td><b>{{$customer->first_name}}</b></td>
           <td><b>{{$customer->last_name}}</b></td>
           <td><b>{{$customer->number}}</b></td>
