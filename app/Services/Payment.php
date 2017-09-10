475bc0fd7bd1d23456851f94d1118730979858f1
Piyush Agrawal <poush12@gmail.com>
2017-09-10 07:10:08 +0530

    payment updated

--- app/Services/Payment.php
+++ app/Services/Payment.php
@@ -20,7 +20,7 @@ class Payment{
 				$receiver = Customer::where('id', $receiver_id)->first();
 				$new_amount = $customer->balance - floatval($amount);
 				$customer->update(['balance' => $new_amount]);
-				dd($receiver->id);
+				dd($receiver_id);
 				$receiver->balance += floatval($amount);
 				$receiver->save();
 				return true;
